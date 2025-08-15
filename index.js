d0aaff625bc6fdba49a906392639cf47115ca6e6
Erika <eri.bertele@gmail.com>
2025-08-15 14:07:17 +0700

    img

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ function rollDice(){
     const diceImages = document.getElementById('diceImages');
     const values = [];
     const images = [];
-
+    const img = document.createElement('img');
     for (let i = 0; i < numOfDice; i++) {
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
         images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
+        diceImages.appendChild(img);
     }
     diceResult.textContent = `dice: ${values.join(', ')}`
     diceImages.innerHTML = images.join('');
